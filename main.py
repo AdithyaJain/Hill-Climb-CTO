e4739ed1be2b12083c4b6645f6268e89cf0c1a8c
AdithyaJain <adithya.jain@research.iiit.ac.in>
2018-12-17 16:07:36 +0530

    added main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 import random
 from observer import *
 from target import *
@@ -7,33 +8,33 @@ env_height = 150
 env_width = 150
 
 # Variables
-no_of_observers = 10
-no_of_targets = 9
+no_of_observers = 12
+no_of_targets = 24
 
-target_speed = 1.2
+target_speed = 0.5
 
 observer_sensor = 15
 target_sensor = 15
 
 
 def distance(coord1, coord2):
-    return math.sqrt((coord1[0] - coord2[0])**2 + (coord1[1] - coord2[1])**2)
+    return np.linalg.norm(coord1 - coord2)
 
 
-def random_destination(position):
-    if position[0] < env_width/4:
-        destination_xx = random.randint(0, math.floor(position[0] + env_width/4))
-    elif position[0] + env_width/4 > env_width:
-        destination_xx = random.randint(math.floor(position[0] - env_width/4), env_width-1)
+def random_destination(position, height, width):
+    if position[0] < width/2:
+        destination_xx = random.randint(0, math.floor(position[0] + width/2))
+    elif position[0] + width/2 > env_width:
+        destination_xx = random.randint(math.floor(position[0] - width/2), env_width-1)
     else:
-        destination_xx = random.randint(math.floor(position[0] - env_width / 4), math.floor(position[0] + env_width/4))
+        destination_xx = random.randint(math.floor(position[0] - width/2), math.floor(position[0] + width/2))
 
-    if position[1] < env_height/4:
-        destination_yy = random.randint(0, math.floor(position[1] + env_height/4))
-    elif position[1] + env_height/4 > env_height:
-        destination_yy = random.randint(math.floor(position[1] - env_height/4), env_height-1)
+    if position[1] < height/2:
+        destination_yy = random.randint(0, math.floor(position[1] + height/2))
+    elif position[1] + height/2 > env_height:
+        destination_yy = random.randint(math.floor(position[1] - height/2), env_height-1)
     else:
-        destination_yy = random.randint(math.floor(position[1] - env_height / 4), math.floor(position[1] + env_height/4))
+        destination_yy = random.randint(math.floor(position[1] - height/2), math.floor(position[1] + height/2))
 
     return destination_xx, destination_yy
 
@@ -41,13 +42,13 @@ def random_destination(position):
 result = 0
 no_of_expts = 100
 
-for exp in range(0, no_of_expts):
+for exp in range(no_of_expts):
     observer = []
-    for i in range(0, no_of_observers):
+    for i in range(no_of_observers):
         observer.append(Observer(i, (random.randint(0, env_width - 1), random.randint(0, env_height - 1))))
 
     target = []
-    for i in range(0, no_of_targets):
+    for i in range(no_of_targets):
         target.append(Target(i, (random.randint(0, env_width - 1), random.randint(0, env_height - 1))))
 
     no_of_targets_observed_per_time_step = 0
@@ -57,60 +58,14 @@ for exp in range(0, no_of_expts):
     #############################
     for timestep in range(0, 1500):
         # COMPUTE OBSERVER'S DESTINATION
-        if timestep % 10 == 0:
-            for obs in observer:
-
                 # Update target list
-                obs.target_list = []
-                for targ in target:
-                    if distance(obs.get_position(), targ.get_position()) <= observer_sensor:
-                        obs.target_list.append(targ.tid)  # TARGET LIST CONTAINS INDEX NUMBER OF TARGET
-
                 # Compute destination
-                # Random
-                if not obs.target_list:
-                    obs.set_destination(random_destination(obs.get_position()))
-                # Mean position of targets
-                else:
-                    sum_target_position_x = sum_target_position_y = 0
-                    length = len(obs.target_list)
-                    for j in obs.target_list:
-                        sum_target_position_x += target[j].get_position()[0]
-                        sum_target_position_y += target[j].get_position()[1]
-                    obs.set_destination((sum_target_position_x/length, sum_target_position_y/length))
 
         # COMPUTE TARGET'S DESTINATION
-        for targ in target:
-            if targ.get_position() == targ.get_destination():
-
                 # Update observer list
-                targ.obs_list = []
-                for obs in observer:
-                    if distance(obs.get_position(), targ.get_position()) <= target_sensor:
-                        targ.obs_list.append(obs.oid)  # OBSERVER LIST CONTAINS INDEX NUMBER OF OBSERVER
-
                 # Compute destination
-                # Random
-                if not targ.obs_list:
-                    targ.set_destination(random_destination(targ.get_position()))
-                # Mean position of observers
-                else:
-                    sum_obs_position_x = sum_obs_position_y = 0
-                    length = len(targ.obs_list)
-                    for j in targ.obs_list:
-                        sum_obs_position_x += observer[j].get_position()[0]
-                        sum_obs_position_y += observer[j].get_position()[1]
-                    mean_obs = (sum_obs_position_x / length, sum_obs_position_y / length)
-                    direction_vector = (targ.get_position()[0] - mean_obs[0], targ.get_position()[1] - mean_obs[1])
-                    if direction_vector == (0, 0):
-                        direction_vector = (random.randint(0, 1) - 0.5, random.randint(0, 1) - 0.5)
-                    magnitude = math.sqrt(direction_vector[0] ** 2 + direction_vector[1] ** 2)
-                    unit_vector = (direction_vector[0] / magnitude, direction_vector[1] / magnitude)
-                    destination_x = targ.get_position()[0] + 100 * unit_vector[0]
-                    destination_y = targ.get_position()[1] + 100 * unit_vector[1]
-                    targ.set_destination((destination_x, destination_y))
 
-        # COMPUTE NO OF TARGETS BEING OBSERVED IN TIMESTEP
+        # COMPUTE NO OF TARGETS BEING OBSERVED IN TIME STEP
         for targ in target:
             if targ.obs_list:
                 no_of_targets_observed_per_time_step += 1
